f56d78bc2dcc4d9a49042755c0d6b86d81bf2219
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-09 20:51:54 +0300

    Добавьте файлы проекта.

---
+++ SortingTask3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+bool check_format(int);
+void sort_vector(std::vector<int>&);
+
+int main()
+{
+    std::vector<int> vec;
+    int input = 0;
+    std::cout << "Fill the vector!\n";
+    bool action = true;
+    while (action)
+    {
+        std::cin >> input;
+        if (input != -1 && input != -2 && check_format(input))
+        {
+            vec.push_back(input);
+        }
+        else if (input == -1)
+        {
+            sort_vector(vec);
+
+        }
+    }
+}
+
+bool check_format(int input)
+{
+    return false;
+}
+
+void sort_vector(std::vector<int>&)
+{
+
+}
