4838f9654a54c739d52044266df2b7fdc3a0ab60
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-09 21:55:13 +0300

    Done!

--- SortingTask3.cpp
+++ SortingTask3.cpp
@@ -1,36 +1,71 @@
 ﻿#include <iostream>
 #include <vector>
 
+
 bool check_format(int);
 void sort_vector(std::vector<int>&);
 
 int main()
 {
     std::vector<int> vec;
-    int input = 0;
-    std::cout << "Fill the vector!\n";
+    int input;
+    std::cout << "Fill the vector!" << std::endl;
     bool action = true;
     while (action)
     {
+        std::cout << "Enter the number: " << std::endl;
         std::cin >> input;
-        if (input != -1 && input != -2 && check_format(input))
+        
+        if (check_format(input))
         {
             vec.push_back(input);
         }
         else if (input == -1)
         {
             sort_vector(vec);
-
+            std::cout << "Fifth number: " << vec[4] << std::endl;
+        }
+        else if (input == -2)
+        {
+            action = false;
+        }
+        else
+        {
+            std::cout << "False input!" << std::endl;
         }
     }
 }
 
 bool check_format(int input)
 {
-    return false;
+    if (input > 9 || input < 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
-void sort_vector(std::vector<int>&)
+void sort_vector(std::vector<int>& vec)
 {
-
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec.size() - i - 1; j++)
+        {
+            if (vec[j] > vec[j + 1])
+            {
+                int temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
+            }
+        }
+    }
 }
+
+
+
+
+        
+    
